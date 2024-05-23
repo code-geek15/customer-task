f96446a8c225efe80f734b36ee5715d6541c66e2
Kgaugelo Mthethwa <121237206+code-geek15@users.noreply.github.com>
2024-05-23 18:28:13 +0200

    customize swagger ui

--- src/main/java/za/co/rmb/customertask/OpenAPISpecs/SwaggerConfig.java
+++ src/main/java/za/co/rmb/customertask/OpenAPISpecs/SwaggerConfig.java
@@ -1,24 +1,38 @@
-//package za.co.rmb.customertask.OpenAPISpecs;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
+package za.co.rmb.customertask.OpenAPISpecs;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Customer Project API")
+                        .version("1.0")
+                        .description("Used this project to get started with Swagger")
+                        .contact(new Contact().name("Kgaugelo Mthethwa")
+                                .email("mthethwakl15@gmail.com")
+                                .url("https://github.com/code-geek15")
+
+                        ));
+    }
+
+
+    // This one doest display anything
 //    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("za.co.rmb.customertask.customer")) // Specify base package for controllers
-//                .paths(PathSelectors.any())
+//    public GroupedOpenApi publicApi() {
+//        return GroupedOpenApi.builder()
+//                .group("public")
+//                .pathsToMatch("/customer")
 //                .build();
 //    }
-//}
+}
